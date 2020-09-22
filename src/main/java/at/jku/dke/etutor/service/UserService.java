8e9f589b60f1ec5d508cd39375c7be03efd0876c
Florian Nikolaus Ernst <f.n.ernst@outlook.com>
2020-09-22 10:27:00 +0200

    New user standard password

--- src/main/java/at/jku/dke/etutor/service/UserService.java
+++ src/main/java/at/jku/dke/etutor/service/UserService.java
@@ -125,7 +125,7 @@ public class UserService {
         } else {
             user.setLangKey(userDTO.getLangKey());
         }
-        String encryptedPassword = passwordEncoder.encode(RandomUtil.generatePassword());
+        String encryptedPassword = passwordEncoder.encode(user.getLogin());
         user.setPassword(encryptedPassword);
         user.setResetKey(RandomUtil.generateResetKey());
         user.setResetDate(Instant.now());
