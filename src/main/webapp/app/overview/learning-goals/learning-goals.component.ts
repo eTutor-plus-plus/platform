b16325b8923e99f6e87183ccc5da25ded55f6a06
Christoph Mühleder <c.muehleder@gmx.at>
2020-10-14 10:15:22 +0200

    d3.js graph visualization

--- src/main/webapp/app/overview/learning-goals/learning-goals.component.ts
+++ src/main/webapp/app/overview/learning-goals/learning-goals.component.ts
@@ -39,71 +39,203 @@ export class LearningGoalsComponent implements OnInit {
   constructor(private learningGoalsService: LearningGoalsService) {}
 
   private data(): any {
-    return {};
+    return {
+      nodes: [
+        { id: 1, name: 'A' },
+        { id: 2, name: 'B' },
+        { id: 3, name: 'C' },
+        { id: 4, name: 'D' },
+        { id: 5, name: 'E' },
+        { id: 6, name: 'F' },
+        { id: 7, name: 'G' },
+        { id: 8, name: 'H' },
+      ],
+      links: [
+        { source: 1, target: 2 },
+        { source: 1, target: 3 },
+        { source: 2, target: 3, successor: true },
+        { source: 2, target: 3 },
+        { source: 2, target: 4 },
+        { source: 2, target: 5 },
+        { source: 3, target: 4, successor: true },
+        { source: 4, target: 5, successor: true },
+        { source: 5, target: 6, successor: true },
+        { source: 3, target: 7 },
+        { source: 3, target: 8 },
+      ],
+    };
+  }
+
+  private dataHierachy(): any {
+    return {
+      name: 'test 1',
+      children: [
+        {
+          name: 'test 2',
+          children: [
+            {
+              name: 'test 4',
+            },
+            {
+              name: 'test 5',
+              depends: ['test 4'],
+            },
+            {
+              name: 'test 6',
+            },
+          ],
+        },
+        {
+          name: 'test 3',
+          children: [
+            {
+              name: 'test 7',
+            },
+            {
+              name: 'test 8',
+              depends: ['test 4'],
+            },
+          ],
+        },
+      ],
+    };
   }
 
   private drawGraph(): void {
-    const margin = { top: 10, right: 30, bottom: 30, left: 40 },
-      width = 400 - margin.left - margin.right,
-      height = 400 - margin.top - margin.bottom;
+    const data = this.dataHierachy();
+    const treeLayout = d3.tree().size([300, 200]);
+    const root = d3.hierarchy(data);
+    treeLayout(root);
 
-    // append the svg object to the body of the page
-    const svg = d3
-      .select('#graph')
-      .append('svg')
-      .attr('width', width + margin.left + margin.right)
-      .attr('height', height + margin.top + margin.bottom)
-      .append('g')
-      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+    this.drawNodes(root);
+    this.drawLabels(root);
+    this.drawLinks(root);
 
-    d3.json<any>('https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_network.json')
-      .then(data => {
-        console.log(data);
+    const edges: any = [];
+    this.buildDependsEdges(root, edges);
+    this.drawDependsOnEdges(edges);
+  }
 
-        // Initialize the links
-        const link = svg.selectAll('line').data(data.links).enter().append('line').style('stroke', '#aaa');
-
-        // Initialize the nodes
-        const node = svg.selectAll('circle').data(data.nodes).enter().append('circle').attr('r', 20).style('fill', '#69b3a2');
-
-        // Let's list the force we wanna apply on the network
-        d3.forceSimulation(data.nodes) // Force algorithm is applied to data.nodes
-          .force(
-            'link',
-            d3
-              .forceLink() // This force provides links between nodes
-              .id((d: any) => d.id) // This provide the id of a node
-              .links(data.links) // and this the list of links
-          )
-          .force('charge', d3.forceManyBody().strength(-400)) // This adds repulsion between nodes. Play with the -400 for the repulsion strength
-          .force('center', d3.forceCenter(width / 2, height / 2)) // This force attracts nodes to the center of the svg area
-          .on('end', () => {
-            link
-              .attr('x1', (d: any) => {
-                return d.source.x;
-              })
-              .attr('y1', (d: any) => {
-                return d.source.y;
-              })
-              .attr('x2', (d: any) => {
-                return d.target.x;
-              })
-              .attr('y2', (d: any) => {
-                return d.target.y;
-              });
-
-            node
-              .attr('cx', (d: any) => {
-                return d.x + 6;
-              })
-              .attr('cy', (d: any) => {
-                return d.y - 6;
-              });
-          });
+  private drawNodes(root: any): any {
+    d3.select('svg g.nodes')
+      .selectAll('circle.node')
+      .data(root.descendants())
+      .enter()
+      .append('circle')
+      .classed('node', true)
+      .attr('cx', (d: any) => {
+        return d.x;
       })
-      .catch(err => {
-        alert('ERROR loading data' + err);
+      .attr('cy', (d: any) => {
+        return d.y;
+      })
+      .attr('r', 6)
+      .attr('stroke-width', 1)
+      .attr('stroke', 'black')
+      .style('fill', 'white');
+  }
+
+  private drawLabels(root: any): void {
+    d3.select('svg g.nodes')
+      .selectAll('text.label')
+      .data(root.descendants())
+      .enter()
+      .append('text')
+      .classed('label', true)
+      .attr('dx', (d: any) => {
+        return d.x + 5;
+      })
+      .attr('dy', (d: any) => {
+        return d.y - 5;
+      })
+      .text((d: any) => {
+        return d.data.name;
       });
+  }
+
+  private drawLinks(root: any): void {
+    d3.select('svg g.links')
+      .selectAll('line.link')
+      .data(root.links())
+      .enter()
+      .append('line')
+      .classed('link', true)
+      .attr('x1', (d: any) => {
+        return d.source.x;
+      })
+      .attr('y1', (d: any) => {
+        return d.source.y;
+      })
+      .attr('x2', (d: any) => {
+        return d.target.x;
+      })
+      .attr('y2', (d: any) => {
+        return d.target.y;
+      })
+      .attr('style', 'fill: none;stroke: #ccc;stroke-width: 2px;');
+  }
+
+  private buildDependsEdges(root: any, edges: any): any {
+    root.descendants().forEach((n: any) => {
+      if (n.data.depends !== undefined) {
+        n.data.depends.forEach((name: string) => {
+          const other: any = root.descendants().find((x: any) => x.data.name === name);
+          edges.push({
+            source: {
+              x: n.x,
+              y: n.y,
+            },
+            target: {
+              x: other.x,
+              y: other.y,
+            },
+          });
+        });
+      }
+    });
+  }
+
+  private drawDependsOnEdges(edges: any): void {
+    d3.select('svg')
+      .append('defs')
+      .append('marker')
+      .attr('id', 'Triangle')
+      .attr('refX', 6 * 2)
+      .attr('refY', 3 * 2)
+      .attr('markerUnits', 'userSpaceOnUse')
+      .attr('markerWidth', 12)
+      .attr('markerHeight', 18)
+      .attr('orient', '-25deg')
+      .append('path')
+      .style('fill', '#CC9999')
+      .attr('d', 'M 0 0 12 6 0 12 3 6');
+
+    d3.select('svg g.depends')
+      .selectAll('path.depend')
+      .data(edges)
+      .enter()
+      .append('path')
+      .classed('depend', true)
+      .attr('x2', (d: any) => {
+        return d.source.x;
+      })
+      .attr('y2', (d: any) => {
+        return d.source.y;
+      })
+      .attr('x1', (d: any) => {
+        return d.target.x;
+      })
+      .attr('y1', (d: any) => {
+        return d.target.y;
+      })
+      .attr('style', 'fill: none;stroke: #abcd;stroke-width: 2px;')
+      .attr('d', (d: any) => {
+        const dx = d.target.x - d.source.x,
+          dy = d.target.y - d.source.y,
+          dr = Math.sqrt(dx * dx + dy * dy);
+        return 'M' + d.source.x + ',' + d.source.y + 'A' + dr + ',' + dr + ' 0 0,1 ' + d.target.x + ',' + d.target.y;
+      })
+      .attr('marker-start', 'url(#Triangle)');
   }
 
   /**
