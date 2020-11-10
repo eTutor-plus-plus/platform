1f45f06ba97bae148a360b54cb249cd31048ea40
FNE <f.n.ernst@outlook.com>
2020-11-10 16:07:39 +0100

    Course management model added.

---
+++ src/main/webapp/app/overview/course-management/course-mangement.model.ts
@@ -0,0 +1,53 @@
+/**
+ * Interface for a course.
+ */
+export interface ICourseModel {
+  /**
+   * The optional course id.
+   */
+  id?: string;
+  /**
+   * The mandatory course name.
+   */
+  name: string;
+  /**
+   * The optional course description.
+   */
+  description?: string;
+  /**
+   * The optional course link.
+   */
+  link?: URL;
+  /**
+   * The mandatory course type.
+   */
+  courseType: string;
+  /**
+   * The optional creator of this course.
+   */
+  creator?: string;
+}
+
+/**
+ * Class which implements the `ICourseModel` interface.
+ */
+export class CourseModel implements ICourseModel {
+  /**
+   * Constructor.
+   *
+   * @param name the mandatory course name
+   * @param courseType the mandatory course type
+   * @param id the optional id
+   * @param description the optional description
+   * @param link the optional link
+   * @param creator the optional creator
+   */
+  constructor(
+    public name: string,
+    public courseType: string,
+    public id?: string,
+    public description?: string,
+    public link?: URL,
+    public creator?: string
+  ) {}
+}
