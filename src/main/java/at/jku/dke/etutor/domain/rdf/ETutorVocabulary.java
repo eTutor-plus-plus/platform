b2479ec93c8afa52f74ef34bf1cc7addf21a9d4a
FNE <f.n.ernst@outlook.com>
2020-12-14 11:37:05 +0100

    Task assignment header added.

--- src/main/java/at/jku/dke/etutor/domain/rdf/ETutorVocabulary.java
+++ src/main/java/at/jku/dke/etutor/domain/rdf/ETutorVocabulary.java
@@ -33,6 +33,7 @@ public final class ETutorVocabulary {
 
     private static final String PROP_HAS_TASK_ASSIGNMENT = "hasTaskAssignment";
     private static final String PROP_HAS_TASK_CREATOR = "hasTaskCreator";
+    private static final String PROP_HAS_TASK_HEADER = "hasTaskHeader";
     private static final String PROP_HAS_TASK_CREATION_DATE = "hasTaskCreationDate";
     private static final String PROP_HAS_TYPICAL_PROCESSING_TIME = "hasTypicalProcessingTime";
     private static final String PROP_HAS_TASK_DIFFICULTY = "hasTaskDifficulty";
@@ -117,6 +118,10 @@ public final class ETutorVocabulary {
      * The hasTaskCreator property.
      */
     public static final Property hasTaskCreator = m.createProperty(URI + PROP_HAS_TASK_CREATOR);
+    /**
+     * The hasTaskHeader property.
+     */
+    public static final Property hasTaskHeader = m.createProperty(URI + PROP_HAS_TASK_HEADER);
     /**
      * The hasTaskCreationDate property.
      */
