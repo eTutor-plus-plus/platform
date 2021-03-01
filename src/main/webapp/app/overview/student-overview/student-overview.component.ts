d1a4463158841ef219ab19a5ff6e2eab1b738462
Florian Nikolaus Ernst <f.n.ernst@outlook.com>
2021-03-01 15:56:34 +0100

    Student overview -> assignments button added.

--- src/main/webapp/app/overview/student-overview/student-overview.component.ts
+++ src/main/webapp/app/overview/student-overview/student-overview.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { StudentService } from '../shared/students/student-service';
 import { CourseInstanceInformationDTO } from '../shared/students/students.model';
 import { Term } from '../course-management/course-mangement.model';
+import { Router } from '@angular/router';
 
 /**
  * Component for displaying a student's overview page.
@@ -18,8 +19,9 @@ export class StudentOverviewComponent implements OnInit {
    * Constructor.
    *
    * @param studentService the injected student service
+   * @param router the injected routing service
    */
-  constructor(private studentService: StudentService) {}
+  constructor(private studentService: StudentService, private router: Router) {}
 
   /**
    * Implements the init method. See {@link OnInit}.
@@ -36,4 +38,11 @@ export class StudentOverviewComponent implements OnInit {
   public getTermI18NStringFromURL(url: string): string {
     return Term.fromString(url)!.text;
   }
+
+  /**
+   * Navigates to the given course instance's assignments.
+   *
+   * @param course the course instance
+   */
+  public navigateToCourseAssignments(course: CourseInstanceInformationDTO): void {}
 }
