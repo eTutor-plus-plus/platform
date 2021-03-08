81b1db500183a2e3d1d50e55fa5801e10c1088fa
FNE <f.n.ernst@outlook.com>
2021-03-08 17:18:03 +0100

    Component for displaying students' assignments added.

--- src/main/webapp/app/overview/overview.module.ts
+++ src/main/webapp/app/overview/overview.module.ts
@@ -4,11 +4,12 @@ import { RouterModule } from '@angular/router';
 import { overviewRoute } from './overview.route';
 import { OverviewComponent } from './overview.component';
 import { StudentOverviewComponent } from './student-overview/student-overview.component';
+import { CourseTaskOverviewComponent } from './student-overview/course-task-overview/course-task-overview.component';
 /**
  * Module for the overview component.
  */
 @NgModule({
   imports: [EtutorPlusPlusSharedModule, RouterModule.forChild(overviewRoute)],
-  declarations: [OverviewComponent, StudentOverviewComponent],
+  declarations: [OverviewComponent, StudentOverviewComponent, CourseTaskOverviewComponent],
 })
 export class OverviewModule {}
---
+++ src/main/webapp/app/overview/student-overview/course-task-overview/course-task-overview.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+/**
+ * Component for displaying the student's task of a given course instance.
+ */
+@Component({
+  selector: 'jhi-course-task-overview',
+  templateUrl: './course-task-overview.component.html',
+  styleUrls: ['./course-task-overview.component.scss'],
+})
+export class CourseTaskOverviewComponent implements OnInit {
+  constructor() {}
+
+  /**
+   * Implements the init method. See {@link OnInit}.
+   */
+  public ngOnInit(): void {}
+}
