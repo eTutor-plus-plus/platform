58a000832a26bdcfbf4372b4e3d07661f0ff4ec7
Florian Nikolaus Ernst <f.n.ernst@outlook.com>
2021-06-14 17:23:26 +0200

    File download component added (files added).

---
+++ src/main/webapp/app/overview/shared/file-download/file-download.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+/**
+ * Component for displaying a downloadable file attachment
+ */
+@Component({
+  selector: 'jhi-file-download',
+  templateUrl: './file-download.component.html',
+  styleUrls: ['./file-download.component.scss']
+})
+export class FileDownloadComponent {
+
+  constructor() { }
+
+}
