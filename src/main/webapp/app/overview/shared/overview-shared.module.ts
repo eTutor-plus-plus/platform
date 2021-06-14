abc1a27439a70133cf34833a151be3362809adac
Florian Nikolaus Ernst <f.n.ernst@outlook.com>
2021-06-14 17:20:25 +0200

    File download component added.

--- src/main/webapp/app/overview/shared/overview-shared.module.ts
+++ src/main/webapp/app/overview/shared/overview-shared.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from 'app/shared/shared.module';
 import { LearningGoalTreeviewComponent } from './learning-goal-treeview/learning-goal-treeview.component';
 import { FileUploadComponent } from 'app/overview/shared/file-upload/file-upload.component';
+import { FileDownloadComponent } from "app/overview/shared/file-download/file-download.component";
 
 /**
  * Module for shared overview components.
  */
 @NgModule({
   imports: [SharedModule],
-  declarations: [LearningGoalTreeviewComponent, FileUploadComponent],
-  exports: [LearningGoalTreeviewComponent, FileUploadComponent],
+  declarations: [LearningGoalTreeviewComponent, FileUploadComponent, FileDownloadComponent],
+  exports: [LearningGoalTreeviewComponent, FileUploadComponent, FileDownloadComponent],
 })
 export class OverviewSharedModule {}
