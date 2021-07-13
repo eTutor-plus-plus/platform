2163b0b5583a1c262d759f3b6dec66a415a68c04
Florian Nikolaus Ernst <f.n.ernst@outlook.com>
2021-07-13 16:28:38 +0200

    Routing handler added.

--- src/main/webapp/app/overview/lecturer-overview/course-instance-statistics-overview/course-instance-statistics-overview.component.ts
+++ src/main/webapp/app/overview/lecturer-overview/course-instance-statistics-overview/course-instance-statistics-overview.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { LecturerOverviewService } from '../lecturer-overview.service';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 /**
  * Component which is used for displaying a statistical overview
@@ -10,11 +12,31 @@ import { LecturerOverviewService } from '../lecturer-overview.service';
   templateUrl: './course-instance-statistics-overview.component.html',
   styleUrls: ['./course-instance-statistics-overview.component.scss'],
 })
-export class CourseInstanceStatisticsOverviewComponent {
+export class CourseInstanceStatisticsOverviewComponent implements OnInit, OnDestroy {
+  private _activatedRouteSubscription?: Subscription;
+  private _instanceId = '';
+
   /**
    * Constructor.
    *
    * @param lecturerOverviewService the injected lecturer overview service
+   * @param activatedRoute the injected activated route service
    */
-  constructor(private lecturerOverviewService: LecturerOverviewService) {}
+  constructor(private lecturerOverviewService: LecturerOverviewService, private activatedRoute: ActivatedRoute) {}
+
+  /**
+   * Implements the init method. See {@link OnInit}.
+   */
+  public ngOnInit(): void {
+    this._activatedRouteSubscription = this.activatedRoute.paramMap.subscribe(paramMap => {
+      this._instanceId = paramMap.get('instanceId')!;
+    });
+  }
+
+  /**
+   * Implements the destroy method. See {@link OnDestroy}.
+   */
+  public ngOnDestroy(): void {
+    this._activatedRouteSubscription?.unsubscribe();
+  }
 }
