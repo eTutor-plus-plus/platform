fc156c8b2df54a6e9045b243d2af9465e8bc0e03
kevschuetz <kevin.hotz@gmx.at>
2021-08-23 14:05:49 +0200

    Dispatcher Routing

--- src/main/webapp/app/overview/dispatcher/dispatcher.module.ts
+++ src/main/webapp/app/overview/dispatcher/dispatcher.module.ts
@@ -4,12 +4,13 @@
 import { SharedModule } from 'app/shared/shared.module';
 import { NgModule } from '@angular/core';
 import { MonacoEditorModule } from 'ngx-monaco-editor';
-
 import { DispatcherComponent } from 'app/overview/dispatcher/dispatcher.component';
 import { AssignmentComponent } from 'app/overview/dispatcher/assignment/assignment.component';
+import { RouterModule } from '@angular/router';
+import { dispatcherRoute } from './dispatcher.route';
 
 @NgModule({
-  imports: [SharedModule, MonacoEditorModule.forRoot()],
+  imports: [SharedModule, MonacoEditorModule.forRoot(), RouterModule.forChild(dispatcherRoute)],
   declarations: [DispatcherComponent, AssignmentComponent],
   exports: [AssignmentComponent],
 })
