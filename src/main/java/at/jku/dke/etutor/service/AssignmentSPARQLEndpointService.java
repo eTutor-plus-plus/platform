301352e963d88b80fe5cc4cff3e82a97c66aecde
kevschuetz <kevin.hotz@gmx.at>
2023-06-07 13:11:11 +0200

    student assignment bug fix validator of form-control was string

--- src/main/java/at/jku/dke/etutor/service/AssignmentSPARQLEndpointService.java
+++ src/main/java/at/jku/dke/etutor/service/AssignmentSPARQLEndpointService.java
@@ -326,16 +326,16 @@ public /*non-sealed*/ class  AssignmentSPARQLEndpointService extends AbstractSPA
                 query.append(".\n");
             }
 
-/** start apriori   */            
-    
+/** start apriori   */
+
             if (StringUtils.isNotBlank(taskAssignment.getAprioriDatasetId())) {
                 query.append("?assignment etutor:hasAprioriID ");
                 query.appendLiteral(taskAssignment.getAprioriDatasetId().trim());
                 query.append(".\n");
             }
-            
+
 /** apriori end */
-            
+
             if(StringUtils.isNotBlank(taskAssignment.getMaxPoints())){
                 query.append("?assignment etutor:hasMaxPoints ");
                 query.appendLiteral(taskAssignment.getMaxPoints());
@@ -701,7 +701,7 @@ public /*non-sealed*/ class  AssignmentSPARQLEndpointService extends AbstractSPA
     public Optional<Integer> getFileIdOfCalcSolution (String assignmentId) {
         Objects.requireNonNull(assignmentId);
         try {
-            return Optional.of(getTaskAssignmentByInternalId(assignmentId).get().getCalcSolutionFileId());
+            return getTaskAssignmentByInternalId(assignmentId).map(NewTaskAssignmentDTO::getCalcSolutionFileId);
         }
         catch (Exception e) {
             return Optional.empty();
@@ -908,14 +908,14 @@ public /*non-sealed*/ class  AssignmentSPARQLEndpointService extends AbstractSPA
         return new TaskGroupDTO(newTaskGroupDTO.getName(), newTaskGroupDTO.getDescription(), newTaskGroupDTO.getTaskGroupTypeId(),
             newTaskGroupDTO.getSqlCreateStatements(), newTaskGroupDTO.getSqlInsertStatementsSubmission(),
             newTaskGroupDTO.getSqlInsertStatementsDiagnose(), newTaskGroupDTO.getxQueryDiagnoseXML(), newTaskGroupDTO.getxQuerySubmissionXML(), newTaskGroupDTO.getDatalogFacts(), resource.getURI(), creator, now
-            
-/** start apriori   */  
+
+/** start apriori   */
             ,
             newTaskGroupDTO.getAprioriID()
-            
+
 /** apriori end */
-        		
-        		
+
+
         		);
     }
 
@@ -1614,16 +1614,16 @@ public /*non-sealed*/ class  AssignmentSPARQLEndpointService extends AbstractSPA
             taskGroupResource.addProperty(ETutorVocabulary.hasDatalogFacts, newTaskGroupDTO.getDatalogFacts().trim());
         }
 
- 
-/** start apriori   */ 
+
+/** start apriori   */
         if (StringUtils.isNotBlank(newTaskGroupDTO.getAprioriID())) {
             taskGroupResource.addProperty(ETutorVocabulary.hasAprioriID, newTaskGroupDTO.getAprioriID().trim());
         }
-        
-        
+
+
 /** apriori end */
-        
-        
+
+
         taskGroupResource.addProperty(ETutorVocabulary.hasTaskGroupCreator, creator);
         taskGroupResource.addProperty(ETutorVocabulary.hasTaskGroupChangeDate, instantToRDFString(creationDate), XSDDatatype.XSDdateTime);
         return taskGroupResource;
@@ -1678,16 +1678,16 @@ public /*non-sealed*/ class  AssignmentSPARQLEndpointService extends AbstractSPA
         if (StringUtils.isNotBlank(newTaskAssignmentDTO.getTaskIdForDispatcher())) {
             resource.addProperty(ETutorVocabulary.hasTaskIdForDispatcher, newTaskAssignmentDTO.getTaskIdForDispatcher().trim());
         }
-/** start apriori   */  
-        
-        
-        
+/** start apriori   */
+
+
+
         if (StringUtils.isNotBlank(newTaskAssignmentDTO.getAprioriDatasetId() )) {
             resource.addProperty(ETutorVocabulary.hasAprioriID, newTaskAssignmentDTO.getAprioriDatasetId().trim());
-        }        
-        
-        
-        
+        }
+
+
+
 /** apriori end */
         if (StringUtils.isNotBlank(newTaskAssignmentDTO.getSqlSolution())) {
             resource.addProperty(ETutorVocabulary.hasSQLSolution, newTaskAssignmentDTO.getSqlSolution().trim());
